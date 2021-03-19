373d7da8455e0e0d72da16bbc90b2d9065923ddb
VitamiN <scoundrel98@gmail.com>
2021-03-19 22:21:09 +0300

    add new.erb view

--- app.rb
+++ app.rb
@@ -10,7 +10,9 @@ get '/' do
 	erb "Hello! <a href=\"https://github.com/bootstrap-ruby/sinatra-bootstrap\">Original</a> pattern has been modified for <a href=\"http://rubyschool.us/\">Ruby School!!!</a>"			
 end
 
-
+get '/new' do
+	erb :new
+end
 
 post '/' do	
 
