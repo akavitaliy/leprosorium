41946d94882c189281582306a73367b866c8caa7
VitamiN <scoundrel98@gmail.com>
2021-03-20 00:19:51 +0300

    redirect new post to index

--- app.rb
+++ app.rb
@@ -43,6 +43,8 @@ post '/new' do
 	#сохранение данных в БД
 	@db.execute 'insert into Posts (content, created_date) values (?,datetime())', [content]
 
-	erb "You type #{content}"
+	#перенаправляет на главную стр.
+	redirect to '/'
+	#erb "You type #{content}"
 end
 
