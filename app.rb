3c1bdc66d7ae130666a4d66623befb309f81fede
VitamiN <scoundrel98@gmail.com>
2021-03-19 22:05:24 +0300

    add sinatra-bootstrap

--- app.rb
+++ app.rb
@@ -7,7 +7,7 @@ require 'sinatra/contrib'
 
 
 get '/' do
-	
+	erb "Hello! <a href=\"https://github.com/bootstrap-ruby/sinatra-bootstrap\">Original</a> pattern has been modified for <a href=\"http://rubyschool.us/\">Ruby School!!!</a>"			
 end
 
 
